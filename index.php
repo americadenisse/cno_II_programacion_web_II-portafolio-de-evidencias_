51ea3142f55301b4af924537d390ce80ef709ec6
americadenisse <ramirez.alvarez.america@gmail.com>
2025-09-22 07:42:31 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Menú de Prácticas</title>
+</head>
+<body>
+<h1>Menú de Ejercicios PHP</h1>
+<ul>
+    <li><a href="ejercicio1.php">Ejercicio 1: PHP Info</a></li>
+    <li><a href="ejercicio2.php">Ejercicio 2: Hola Mundo</a></li>
+    <li><a href="ejercicio3.php">Ejercicio 3: Hola Mundo con formato</a></li>
+    <li><a href="ejercicio4.php">Ejercicio 4: Variables</a></li>
+    <li><a href="ejercicio5.php">Ejercicio 5: Concatenación</a></li>
+    <li><a href="ejercicio6.php">Ejercicio 6: Variables con formato</a></li>
+    <li><a href="tarea1.php">Tarea 1: Canción Favorita</a></li>
+</ul>
+</body>
+</html>
